d89ce81fb4642f23050b1b20d4884032c10e34f1
zobayerdev <zobayer.dev@gmail.com>
2024-09-26 17:12:03 +0600

    device preview uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebases_flutter/crud_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:device_preview/device_preview.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),);
 }
+
+
+
 
 class MyApp extends StatelessWidget {
   var auth = FirebaseAuth.instance;
@@ -18,6 +26,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Flutter CRUD',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
